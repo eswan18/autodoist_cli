e6b082984bbc4611ae114a6fe2c599229216874d
Ethan Swan <eswan18@rocketmail.com>
2019-11-22 06:41:22 -0500

    Add test of interactive travel checklist creation.

--- tests/test_travel_checklist.py
+++ tests/test_travel_checklist.py
@@ -1,6 +1,18 @@
 from autodoist import cli
+from pathlib import Path
 from click.testing import CliRunner
 
 def test_general_invocation():
     runner = CliRunner()
-    runner.invoke(cli, ['travel-checklist'])
+    result = runner.invoke(cli, ['travel-checklist'])
+    assert result.exit_code == 0
+
+def test_create_invocation():
+    OUT_FILE = 'test-trip.csv'
+    runner = CliRunner()
+    result = runner.invoke(cli, ['travel-checklist', 'create'],
+                           input='''test-trip\n{}
+                                    7\nn\ny\n1
+                                    78\n45\ny\ny'''.format(OUT_FILE))
+    assert result.exit_code == 0
+    assert Path(OUT_FILE).exists()
