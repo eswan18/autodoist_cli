7dbb6b858034c09fa614cadf6810b8c61f877fde
Ethan Swan <eswan18@rocketmail.com>
2019-11-22 07:52:55 -0500

    Create parametrized test.

--- tests/test_travel_checklist.py
+++ tests/test_travel_checklist.py
@@ -1,18 +1,33 @@
 from autodoist import cli
 from pathlib import Path
 from click.testing import CliRunner
+import pytest
+import json
+
+KEY_ORDER = ['name', 'csv_name', 'n_days', 'can_wash', 'is_flying',
+             'dress_days', 'max_temp', 'min_temp', 'extended_outdoor_time']
+
+# Load trips to use as parameters.
+with open('tests/data/trips.json') as f:
+    trips = json.loads(f.read())
 
 def test_general_invocation():
     runner = CliRunner()
     result = runner.invoke(cli, ['travel-checklist'])
     assert result.exit_code == 0
 
-def test_create_invocation():
-    OUT_FILE = 'test-trip.csv'
+@pytest.mark.parametrize('trip', trips)
+def test_create_interactive_invocation(trip):
+    spec = trip['spec']
     runner = CliRunner()
+    # Create the user input.
+    input = '\n'.join([str(spec[key]) for key in KEY_ORDER])
+    # We may be prompted to overwrite a file.
+    out_path = Path(spec['csv_name'])
+    if out_path.exists():
+        input += '\ny'
     result = runner.invoke(cli, ['travel-checklist', 'create'],
-                           input='''test-trip\n{}
-                                    7\nn\ny\n1
-                                    78\n45\ny\ny'''.format(OUT_FILE))
+                           input=input)
     assert result.exit_code == 0
-    assert Path(OUT_FILE).exists()
+    conf_text = 'CSV written to file: {}'.format(str(out_path.absolute()))
+    assert conf_text in result.output
